ba7b3bc52247055be552c017aecf348241ce30bc
erdem <erdm@disroot.org>
2023-08-04 07:58:53 +0000

    live st

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ let content = document.getElementById("statustext");
 function fetchtrack(){
     fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${lastfm_username}&api_key=${lastfm_apikey}&format=json`).then(response => response.json()).then(e => {
     if(e["recenttracks"]){
+        if (!e.recenttracks.track[0]["@attr"]){
+            content.innerText = "i'm not listening to anything right now"
+            content.href = "https://www.last.fm/user/erdempro"
+            return;
+        }
         content.innerText = `${e.recenttracks.track[0].name} - ${e.recenttracks.track[0].artist["#text"]}`
         content.href = e.recenttracks.track[0].url
     }else{
